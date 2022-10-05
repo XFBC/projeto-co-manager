0ad0f0f02c4826d253e457a287447f748e791a4c
FABRICIO LOPES <92175971+XFBC@users.noreply.github.com>
2022-10-05 20:45:01 -0300

    Edição da pagina de projetos

--- co-manager-react/src/components/layout/Container.jsx
+++ co-manager-react/src/components/layout/Container.jsx
@@ -1,7 +1,7 @@
-export default function Container(props) {
+export default function Container({ children }) {
   return (
     <div className="container min-h-[75%] px-2 d-flex justify-between m-auto  flex-wrap bg-slate-300  w-full">
-      {props.children}
+      {children}
     </div>
   )
 }
--- co-manager-react/src/components/pages/Projeto.jsx
+++ co-manager-react/src/components/pages/Projeto.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import ProjetoForm from '../projeto/ProjetoForm'
 
 export default function Projeto() {
   const { id } = useParams()
-  const [projeto, setProjeto] = useState()
+  const [project, setProject] = useState([])
+  const [showProjectForm, setShowProjectForm] = useState(false)
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/` + id, {
+    fetch(`http://localhost:5000/projects/${id}`, {
       method: 'GET',
       headers: {
         'Content-type': 'application/json'
@@ -15,16 +17,63 @@ export default function Projeto() {
     })
       .then(resp => resp.json())
       .then(data => {
-        setProjeto(data)
+        setProject(data)
       })
 
       .catch(err => console.log(err))
   }, [id])
 
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm)
+  }
+
+  function editPost(project) {
+    //budget validation
+
+    if (project.budget < project.coManager) {
+      //msg
+    }
+
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json'
+      },
+
+      body: JSON.stringify(project)
+    })
+      .then(r => r.json())
+      .then(data => {
+        setProject(data)
+        setShowProjectForm(false)
+      })
+  }
+
   return (
     <div>
-      <h1 className="text-2xl">Projeto: {}  </h1>
-      <button>Editar projeto</button>
+      <h1 className="text-2xl">Projeto: {project.name}</h1>
+      <button
+        className='border-yellow-400 border-x-2 border-y-2 px-2 py-2"'
+        onClick={toggleProjectForm}
+      >
+        {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+      </button>
+
+      {!showProjectForm ? (
+        <>
+          <p>Categoria: {project.category && project.category.name} </p>
+          <p>Budget: {project.budget}</p>
+          <p>Total ultilizado: R$ {project.coManager}</p>
+        </>
+      ) : (
+        <>
+          <ProjetoForm
+            handleSubmit={editPost}
+            btnText="atualizar projeto"
+            projectData={project}
+          />
+        </>
+      )}
     </div>
   )
 }
